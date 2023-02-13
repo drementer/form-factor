e4b2907a54cd2e145fff843a6d1c0f3257feb194
drementer <48869708+drementer@users.noreply.github.com>
2023-02-13 22:11:28 +0300

    added login view

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <form action="/home" class="login__form form" form>
+      <input type="text" value="drementer" required />
+      <input type="password" value="drementer" required />
+      <button>giriş yap</button>
+    </form>
+  </div>
+</template>
+
+<style lang="scss">
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+	height: 100%;
+}
+</style>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
