870e2d9b80820b488d3961811e6bb17fd249088a
drementer <48869708+drementer@users.noreply.github.com>
2023-02-13 22:12:35 +0300

    adding first component

---
+++ src/components/Input.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="input">
+    <label>
+			<span v-if="label">{{ label }}</span>
+      <input
+        :type="type"
+        :name="name"
+        :value="value"
+        :required="required"
+				:placeholder="placeholder"
+				input
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    name: String,
+    value: String,
+		placeholder: String,
+    required: Boolean,
+		label: String,
+  },
+};
+</script>
+
+<style></style>
