60ae301ed5ae44dff9606d0ab2ee8cb21cf88596
drementer <48869708+drementer@users.noreply.github.com>
2023-02-23 17:34:05 +0300

    change al project path

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="home" main>
+    <div class="brand-list">
+      <BrandCard
+        v-for="brand in brands"
+        :image="brand.image"
+        :image-alt="brand.imageAlt"
+      />
+    </div>
+  </main>
+</template>
+
+
+
+<script>
+import Input from '@/components/Input.vue';
+import BrandCard from '@/components/BrandCard.vue';
+
+export default {
+  name: 'HomeView',
+  data: () => {
+    return {
+      brands: [
+        {
+          name: 'Katılımemeklilik',
+          image: "katilimemeklilik-logo.png",
+          imageAlt: 'Katılımemeklilik logo',
+        },
+      ],
+    };
+  },
+  components: {
+    Input,
+    BrandCard,
+  },
+};
+</script>
