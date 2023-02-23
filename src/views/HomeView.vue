e5abb631c83679f73f6fbb2b9e4e77c5253c2723
drementer <48869708+drementer@users.noreply.github.com>
2023-02-23 17:40:31 +0300

    debug by adding key attr

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,13 @@
     <div class="brand-list">
       <BrandCard
         v-for="brand in brands"
+        :key="brand.id"
         :image="brand.image"
         :image-alt="brand.imageAlt"
       />
     </div>
   </main>
 </template>
-
-
 
 <script>
 import Input from '@/components/Input.vue';
@@ -22,8 +21,9 @@ export default {
     return {
       brands: [
         {
+					id: 1,
           name: 'Katılımemeklilik',
-          image: "katilimemeklilik-logo.png",
+          image: 'katilimemeklilik-logo.png',
           imageAlt: 'Katılımemeklilik logo',
         },
       ],
