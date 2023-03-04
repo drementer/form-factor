edb3803a578d8f8fd9bdfc057628f36eb8273017
Burak <48869708+drementer@users.noreply.github.com>
2023-03-04 16:38:27 +0300

    Add homepage splitted rouutes

--- src/router/home.js
+++ src/router/home.js
@@ -1,9 +1,22 @@
 import view from '../views/HomeView.vue';
+import addBrand from '../views/brand/AddBrand.vue';
+import brands from '../views/brand/Brands.vue';
 
 export default [
   {
     path: '/home',
     name: 'home',
     component: view,
+    redirect: '/brands',
+    children: [
+      {
+        path: '/brands',
+        component: brands,
+      },
+      {
+        path: '/brands/add-brand',
+        component: addBrand,
+      },
+    ],
   },
 ];
