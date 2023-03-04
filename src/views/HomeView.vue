2d3aa3a7b952ab18ec1bbfdb163478c60d71a0fb
Burak <48869708+drementer@users.noreply.github.com>
2023-03-04 16:36:52 +0300

    Split subpages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,21 @@
 <template>
   <main class="home" main>
-    <div class="brand-list">
-      <BrandCard
-        v-for="brand in brands"
-        :key="brand.id"
-        :image="brand.image"
-        :image-alt="brand.imageAlt"
-      />
-    </div>
+    <router-view />
   </main>
 </template>
 
-<script>
-import Input from '@/components/Input.vue';
-import BrandCard from '@/components/BrandCard.vue';
-import { getBrands } from '../service/index.js';
+<style lang="scss">
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+  height: 100%;
+}
+</style>
+
+<script>
 export default {
   name: 'HomeView',
-  data: () => {
-    return {
-      brands: [],
-    };
-  },
-  components: {
-    Input,
-    BrandCard,
-  },
-  methods: {
-    getBrands() {
-      getBrands().then((res) => (this.brands = res));
-    },
-  },
-  mounted() {
-    this.getBrands();
-  },
 };
 </script>
---
+++ src/views/brand/Brands.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="brands">
+    <h1 class="brands__heading">Markalar</h1>
+    <div class="brands__cards">
+      <BrandCard
+        v-for="brand in brands"
+        class="brands__card"
+        :key="brand.id"
+        :image="brand.image"
+        :image-alt="brand.imageAlt"
+      />
+    </div>
+    <div class="brands__button">
+      <router-link to="/brands/add-brand" class="brand__button button">
+        Yeni Marka Ekle
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.brands {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-content: center;
+  gap: 1em;
+
+  color: $white;
+  font-size: 1rem;
+
+  &__heading {
+    text-align: center;
+  }
+
+  &__cards {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 1em;
+
+    max-width: 20em;
+  }
+
+  &__card {
+    flex-basis: calc(50% - (1em / 2));
+  }
+}
+</style>
+
+<script>
+import Input from '@/components/Input.vue';
+import BrandCard from '@/components/BrandCard.vue';
+import { getBrands } from '@/service/index.js';
+
+export default {
+  name: 'Brands',
+  data: () => {
+    return {
+      brands: [],
+    };
+  },
+  components: {
+    Input,
+    BrandCard,
+  },
+  methods: {
+    getBrands() {
+      getBrands().then((res) => (this.brands = res));
+    },
+  },
+  mounted() {
+    this.getBrands();
+  },
+};
+</script>
