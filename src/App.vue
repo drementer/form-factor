4536ecf843346edd426be21d03b0f5611ed26475
Burak <48869708+drementer@users.noreply.github.com>
2023-03-05 00:15:21 +0300

    Fix: Layout shift

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <router-view />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
+
+<style lang="scss">
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+}
+</style>
