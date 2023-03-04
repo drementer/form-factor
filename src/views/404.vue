2e3956aecade73bf49c490523bff744e2b764b4f
Burak <48869708+drementer@users.noreply.github.com>
2023-03-04 16:36:11 +0300

    add 404 view

---
+++ src/views/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="login" main>
+    <h2>404</h2>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+* {
+  color: white;
+}
+</style>
+
+<script>
+export default {
+  name: 'Error',
+};
+</script>
