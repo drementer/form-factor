9b5dd4b50b3a9b9e6cd05d7d45756a3b4a4459e0
Burak <48869708+drementer@users.noreply.github.com>
2023-03-06 18:35:20 +0300

    Refactor

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
 
 const getBrands = async () => {
-  const response = await axios.get(`/api/get-brands`);
+  const response = await axios({
+    method: 'get',
+    url: '/api/brand/get-brands',
+  });
+
   return response.data;
 };
 
