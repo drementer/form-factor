e2dbe13117a85a1242a3dd004973673205e8e232
Burak <48869708+drementer@users.noreply.github.com>
2023-03-06 14:36:26 +0300

    Deleted unused services

--- src/service/index.js
+++ src/service/index.js
@@ -1,18 +1,8 @@
 const axios = require('axios');
 
-const getUsers = async () => {
-  const response = await axios.get('/api/users');
-  return response.data;
-};
-
-const createUser = async (data) => {
-  const response = await axios.post(`/api/user`, data);
-  return response.data;
-};
-
 const getBrands = async () => {
   const response = await axios.get(`/api/get-brands`);
   return response.data;
 };
 
-export { createUser, getUsers, getBrands };
+export { getBrands };
