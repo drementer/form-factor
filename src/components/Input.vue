32d1ecad61db33c7aae8d21860b229432a9ccd83
drementer <48869708+drementer@users.noreply.github.com>
2023-03-26 16:15:00 +0300

    Update: font size status

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -108,7 +108,7 @@ export default {
   padding: 0;
 
   color: $white;
-  font-size: 1.2rem;
+  font-size: 1.125rem;
 
   .-muted {
     opacity: 0.5;
@@ -178,6 +178,7 @@ export default {
     padding: 0;
 
     font-size: 0.8em;
+    opacity: 0.8;
   }
 }
 </style>
