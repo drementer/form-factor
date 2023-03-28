669d834a7a65a4a579a1710416a893c34c2cf42b
drementer <48869708+drementer@users.noreply.github.com>
2023-03-28 21:15:42 +0300

    Add: Button moddifier to button

--- src/views/brand/AddBrand.vue
+++ src/views/brand/AddBrand.vue
@@ -41,10 +41,10 @@
         form-item
       />
       <div class="form__buttons">
-				<button class="button form__button" type="reset" form-button="reset">
+				<button class="button form__button -wide" type="reset" form-button="reset">
 					Temizle
 				</button>
-        <button class="button form__button" type="submit" form-button="submit">
+        <button class="button form__button -wide" type="submit" form-button="submit">
           marka ekle
         </button>
       </div>
