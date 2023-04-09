a26d6807fd8c80861c2b831254feee890b58e8c3
drementer <48869708+drementer@users.noreply.github.com>
2023-04-09 12:50:57 +0300

    Add: Bse brand template

---
+++ src/views/brand/Brand.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="brand">
+    {{ brandId }}
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Brand',
+  data: function () {
+    return {
+      brandId: this.$route.params.id,
+    };
+  },
+  components: {},
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss"></style>
